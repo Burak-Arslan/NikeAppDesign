6f68271707642750e3529a36e2bd44fe75498bf6
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2021-04-23 15:55:22 +0300

    - initial

---
+++ app/src/main/java/com/example/nikedesign/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nikedesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
