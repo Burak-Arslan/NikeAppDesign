f782b2f0d96fe047ab3ac26a39d59e3f5ef53a5e
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2021-04-24 12:56:51 +0300

    - Ana ekran düzenlemeleri ayarlandı- -Datalar ayarlandı

--- app/src/main/java/com/example/nikedesign/MainActivity.kt
+++ app/src/main/java/com/example/nikedesign/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.nikedesign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
